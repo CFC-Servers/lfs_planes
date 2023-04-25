887d26b450882b82c4f8d0ebc01ab67821777c16
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-04-24 17:11:01 -0700

    Fix and optimize vehicle spanwer (except scoping)

--- lua/entities/lfs_vehicle_spammer.lua
+++ lua/entities/lfs_vehicle_spammer.lua
@@ -13,35 +13,41 @@ ENT.Spawnable		= true
 ENT.AdminOnly		= true
 ENT.Editable = true
 
+local Vector = Vector
+local CurTime = CurTime
+local IsValid = IsValid
+local isnumber = isnumber
+
+local TEXT_ALIGN_CENTER = TEXT_ALIGN_CENTER
+
+local RED = Color( 255, 0, 0, 255 )
+local AZURE = Color( 0, 127, 255, 255 )
+local WHITE = Color( 255, 255, 255, 255 )
+
+-- TODO: Simplify this loop
 function ENT:SetupDataTables()
 	local AllSents = scripted_ents.GetList() 
 	local SpawnOptions = {}
 
-	for _, v in pairs( AllSents ) do
-		if v and istable( v.t ) then
-			if v.t.Spawnable then
-				if v.t.Base and string.StartWith( v.t.Base:lower(), "lunasflightschool_basescript" ) then
-					if v.t.Category and v.t.PrintName then
-						local nicename = v.t.Category.." - "..v.t.PrintName
-						if not table.HasValue( SpawnOptions, nicename ) then
-							SpawnOptions[nicename] = v.t.ClassName
-						end
-					end
-				end
+	for _, v in ipairs( AllSents ) do
+		if v and istable( v.t ) and v.t.Spawnable and ( v.t.Base and string.StartWith( v.t.Base:lower(), "lunasflightschool_basescript" ) ) and ( v.t.Category and v.t.PrintName ) then
+			local nicename = v.t.Category .. " - " .. v.t.PrintName
+			if not table.HasValue( SpawnOptions, nicename ) then
+				SpawnOptions[nicename] = v.t.ClassName
 			end
 		end
 	end
 
-	self:NetworkVar( "String",0, "Type",	{ KeyName = "Vehicle Type",Edit = { type = "Combo",	order = 1,values = SpawnOptions,category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",3, "TeamOverride", { KeyName = "AI Team", Edit = { type = "Int", order = 4,min = -1, max = 3, category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",4, "RespawnTime", { KeyName = "spawntime", Edit = { type = "Int", order = 5,min = 1, max = 120, category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",5, "Amount", { KeyName = "amount", Edit = { type = "Int", order = 6,min = 1, max = 10, category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",6, "SpawnWithSkin", { KeyName = "spawnwithskin", Edit = { type = "Int", order = 8,min = 0, max = 16, category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",7, "SpawnWithHealth", { KeyName = "spawnwithhealth", Edit = { type = "Int", order = 9,min = 0, max = 50000, category = "Vehicle-Options"} } )
-	self:NetworkVar( "Int",8, "SpawnWithShield", { KeyName = "spawnwithshield", Edit = { type = "Int", order = 10,min = 0, max = 50000, category = "Vehicle-Options"} } )
+	self:NetworkVar( "String",0, "Type", { KeyName = "Vehicle Type", Edit = { type = "Combo", order = 1, values = SpawnOptions, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",3, "TeamOverride", { KeyName = "AI Team", Edit = { type = "Int", order = 4, min = -1, max = 3, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",4, "RespawnTime", { KeyName = "spawntime", Edit = { type = "Int", order = 5, min = 1, max = 120, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",5, "Amount", { KeyName = "amount", Edit = { type = "Int", order = 6, min = 1, max = 10, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",6, "SpawnWithSkin", { KeyName = "spawnwithskin", Edit = { type = "Int", order = 8, min = 0, max = 16, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",7, "SpawnWithHealth", { KeyName = "spawnwithhealth", Edit = { type = "Int", order = 9, min = 0, max = 50000, category = "Vehicle-Options"} } )
+	self:NetworkVar( "Int",8, "SpawnWithShield", { KeyName = "spawnwithshield", Edit = { type = "Int", order = 10, min = 0, max = 50000, category = "Vehicle-Options"} } )
 
-	self:NetworkVar( "Int",9, "UseHealth", { KeyName = "maxhealth", Edit = { type = "Int", order = 21,min = 0, max = 50000, category = "Spawner-Options"} } )
-	self:NetworkVar( "Int",10, "SelfDestructAfterAmount", { KeyName = "selfdestructafteramount", Edit = { type = "Int", order = 22,min = 0, max = 100, category = "Spawner-Options"} } )
+	self:NetworkVar( "Int",9, "UseHealth", { KeyName = "maxhealth", Edit = { type = "Int", order = 21, min = 0, max = 50000, category = "Spawner-Options"} } )
+	self:NetworkVar( "Int",10, "SelfDestructAfterAmount", { KeyName = "selfdestructafteramount", Edit = { type = "Int", order = 22, min = 0, max = 100, category = "Spawner-Options"} } )
 	self:NetworkVar( "Bool",2, "MasterSwitch" )
 
 	if SERVER then
@@ -58,8 +64,10 @@ function ENT:SetupDataTables()
 end
 
 if SERVER then
-	function ENT:SpawnFunction( ply, tr, ClassName )
+	local spawnPosOffset = Vector( 0, 500, 150 ) 
+	local spawnAngOffset = Angle( 0, -90, 0 )
 
+	function ENT:SpawnFunction( ply, tr, ClassName )
 		if not tr.Hit then return end
 
 		local ent = ents.Create( ClassName )
@@ -67,9 +75,7 @@ if SERVER then
 		ent:SetPos( tr.HitPos + tr.HitNormal * 1 )
 		ent:Spawn()
 		ent:Activate()
-
 		return ent
-
 	end
 
 	function ENT:GetHP()
@@ -82,7 +88,6 @@ if SERVER then
 
 	function ENT:OnUseHealthChanged( name, old, new)
 		if new == old then return end
-
 		self:SetHP( new )
 	end
 
@@ -93,23 +98,21 @@ if SERVER then
 
 		local Damage = dmginfo:GetDamage()
 		local CurHealth = self:GetHP()
-		local NewHealth = math.Clamp( CurHealth - Damage , 0, self:GetUseHealth() )
+		local NewHealth = math.Clamp( CurHealth - Damage, 0, self:GetUseHealth() )
 
 		self:SetHP( NewHealth )
 
-		if NewHealth <= 0 then
-			if not self.MarkForRemove then
-				self.MarkForRemove =  true
-	
-				for i = -5, 5 do
-					local Pos = self:LocalToWorld( Vector(0,i * 100,10)  )
+		if NewHealth <= 0 and ( not self.MarkForRemove ) then
+			self.MarkForRemove =  true
 
-					timer.Simple( math.Rand(0,1), function()
-						local effectdata = EffectData()
-							effectdata:SetOrigin( Pos )
-						util.Effect( "lfs_explosion_nodebris", effectdata )
-					end)
-				end
+			for i = -5, 5 do
+				local Pos = self:LocalToWorld( Vector( 0, i * 100, 10 ) )
+
+				timer.Simple( math.Rand( 0, 1 ), function()
+					local effectdata = EffectData()
+					effectdata:SetOrigin( Pos )
+					util.Effect( "lfs_explosion_nodebris", effectdata )
+				end )
 			end
 		end
 	end
@@ -136,7 +139,9 @@ if SERVER then
 		end
 	end
 	
+	-- TODO: Heavily optimize this
 	function ENT:Think()
+		-- TODO: Extract to helper method
 		if IsValid( self.Defusor ) and isnumber( self.DefuseTime ) then
 			if self.Defusor:KeyDown( IN_USE ) then
 				if CurTime() - self.DefuseTime > 1 then
@@ -149,9 +154,9 @@ if SERVER then
 					end
 
 					if self:GetMasterSwitch() then
-						self.Defusor:PrintMessage( HUD_PRINTTALK, "ALL AI-Spawners Enabled")
+						self.Defusor:PrintMessage( HUD_PRINTTALK, "ALL AI-Spawners Enabled" )
 					else
-						self.Defusor:PrintMessage( HUD_PRINTTALK, "ALL AI-Spawners Disabled")
+						self.Defusor:PrintMessage( HUD_PRINTTALK, "ALL AI-Spawners Disabled" )
 					end
 
 					self.Defusor = nil
@@ -160,9 +165,9 @@ if SERVER then
 				self:SetMasterSwitch( not self:GetMasterSwitch() )
 
 				if self:GetMasterSwitch() then
-					self.Defusor:PrintMessage( HUD_PRINTTALK, "AI-Spawner Enabled")
+					self.Defusor:PrintMessage( HUD_PRINTTALK, "AI-Spawner Enabled" )
 				else
-					self.Defusor:PrintMessage( HUD_PRINTTALK, "AI-Spawner Disabled")
+					self.Defusor:PrintMessage( HUD_PRINTTALK, "AI-Spawner Disabled" )
 				end
 
 				self.Defusor = nil
@@ -171,27 +176,26 @@ if SERVER then
 
 		if self.MarkForRemove then
 			self:Remove()
-
 			return
 		end
 
 		if not self:GetMasterSwitch() then return end
 
 		self.spawnedvehicles = self.spawnedvehicles or {}
-		
+
+		-- TODO: Extract to helper function
 		if self.ShouldSpawn then
 			if self.NextSpawn < CurTime() then
-				
 				self.ShouldSpawn = false
-				
-				local pos = self:LocalToWorld( Vector( 0, 500, 150 ) )
-				local ang = self:LocalToWorldAngles( Angle( 0, -90, 0 ) )
-				
+
+				local pos = self:LocalToWorld( spawnPosOffset )
+				local ang = self:LocalToWorldAngles( spawnAngOffset )
+
 				local Type = self:GetType()
-				
+
 				if Type ~= "" then
 					local spawnedvehicle = ents.Create( Type )
-					
+
 					if IsValid( spawnedvehicle ) then
 						spawnedvehicle:SetPos( pos )
 						spawnedvehicle:SetAngles( ang )
@@ -243,15 +247,15 @@ if SERVER then
 					self.spawnedvehicles[k] = nil
 				end
 			end
-			
+
 			if AmountSpawned < self:GetAmount() then
 				self.ShouldSpawn = true
 				self.NextSpawn = CurTime() + self:GetRespawnTime()
 			end
 		end
-		
+
 		self:NextThink( CurTime() )
-		
+
 		return true
 	end
 end
@@ -259,6 +263,7 @@ end
 if CLIENT then
 	local mat = Material( "sprites/light_glow02_add" )
 	
+	-- TODO: Reduce the amount of new Vectors created
 	function ENT:Draw()
 		self:DrawModel()
 
@@ -272,11 +277,11 @@ if CLIENT then
 
 		if not self:GetMasterSwitch() then
 			render.SetMaterial( mat )
-			render.DrawSprite( self:LocalToWorld( Vector(125,500 - self.PX,10) ), 32, 32, Color( 255, 255, 255, 255) )
-			render.DrawSprite( self:LocalToWorld( Vector(-125,500 - self.PX,10) ), 32, 32, Color( 255, 255, 255, 255) )
+			render.DrawSprite( self:LocalToWorld( Vector( 125, 500 - self.PX,10 ) ), 32, 32, WHITE )
+			render.DrawSprite( self:LocalToWorld( Vector( -125, 500 - self.PX,10 ) ), 32, 32, WHITE )
 			
-			render.DrawSprite( self:LocalToWorld( Vector(125,500 - self.PX,10) ), 130, 130, Color( 255, 0, 0, 255) )
-			render.DrawSprite( self:LocalToWorld( Vector(-125,500 - self.PX,10) ), 130, 130, Color( 255, 0, 0, 255) )
+			render.DrawSprite( self:LocalToWorld( Vector( 125, 500 - self.PX, 10 ) ), 130, 130, RED )
+			render.DrawSprite( self:LocalToWorld( Vector( -125, 500 - self.PX, 10 ) ), 130, 130, RED )
 			
 			return
 		end
@@ -284,15 +289,16 @@ if CLIENT then
 		if self:GetType() ~= "" then return end
 
 		render.SetMaterial( mat )
-		render.DrawSprite( self:LocalToWorld( Vector(125,500 - self.PX,10) ), 32, 32, Color( 255, 255, 255, 255) )
-		render.DrawSprite( self:LocalToWorld( Vector(-125,500 - self.PX,10) ), 32, 32, Color( 255, 255, 255, 255) )
+		render.DrawSprite( self:LocalToWorld( Vector( 125, 500 - self.PX, 10 ) ), 32, 32, WHITE )
+		render.DrawSprite( self:LocalToWorld( Vector( -125, 500 - self.PX, 10 ) ), 32, 32, WHITE )
 		
-		render.DrawSprite( self:LocalToWorld( Vector(125,500 - self.PX,10) ), 130, 130, Color( 0, 127, 255, 255) )
-		render.DrawSprite( self:LocalToWorld( Vector(-125,500 - self.PX,10) ), 130, 130, Color( 0, 127, 255, 255) )
+		render.DrawSprite( self:LocalToWorld( Vector( 125, 500 - self.PX, 10 ) ), 130, 130, AZURE )
+		render.DrawSprite( self:LocalToWorld( Vector( -125, 500 - self.PX, 10 ) ), 130, 130, AZURE )
 	end
 
 	local TutorialDone = false
 
+	-- TODO: Can we remove this entirely?
 	hook.Add( "HUDPaint", "!!!!!!!11111lfsvehiclespammer_tutorial", function()
 		if TutorialDone then return end
 
@@ -301,28 +307,26 @@ if CLIENT then
 		if ply:InVehicle() then return end
 
 		local trace = ply:GetEyeTrace()
-		local Dist = (ply:GetShootPos() - trace.HitPos):Length()
+		local Dist = ( ply:GetShootPos() - trace.HitPos ):Length()
 
 		if Dist > 800 then return end
 
 		local Ent = trace.Entity
-
 		if not IsValid( Ent ) then return end
 
 		if Ent:GetClass() == "lfs_vehicle_spammer" then
 			local pos = Ent:GetPos()
 			local scr = pos:ToScreen()
-			local Alpha = 255
 
 			if Ent:GetType() == "" then
-				draw.SimpleText( "Hold C => Right Click on me => Edit Properties => Choose a Type", "LFS_FONT", scr.x, scr.y - 10, Color(255,255,255,Alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+				draw.SimpleText( "Hold C => Right Click on me => Edit Properties => Choose a Type", "LFS_FONT", scr.x, scr.y - 10, WHITE, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
 			else
 				if not Ent:GetMasterSwitch() then
 					local Key = input.LookupBinding( "+use" )
 					if not isstring( Key ) then Key = "+use is not bound to a key" end
 
-					draw.SimpleText( "Now press ["..Key.."] to enable!", "LFS_FONT", scr.x, scr.y - 10, Color(255,255,255,Alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-					draw.SimpleText( "or hold ["..Key.."] to enable globally!", "LFS_FONT", scr.x, scr.y + 10, Color(255,255,255,Alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+					draw.SimpleText( "Now press [" .. Key .. "] to enable!", "LFS_FONT", scr.x, scr.y - 10, WHITE, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+					draw.SimpleText( "or hold [" .. Key .. "] to enable globally!", "LFS_FONT", scr.x, scr.y + 10, WHITE, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
 				else
 					TutorialDone = true
 				end
