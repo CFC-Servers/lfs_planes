3fbf2cca88b4e8ff9a1a45cf5f92f057ec7c1cda
Redox <mail@redox.to>
2023-10-26 19:13:11 +0200

    Move and delay conflict finder

---
+++ lua/autorun/lfs_conflictfinder.lua
@@ -0,0 +1,16 @@
+local conflictingAddons = {
+    ["3032439287"] = true
+}
+
+timer.Simple( 60, function()
+    for _, addon in ipairs( engine.GetAddons() ) do
+        local wsid = addon.wsid
+        if conflictingAddons[wsid] then
+            if CLIENT then
+                chat.AddText( Color( 255, 0, 0 ), "[LFS] ", Color( 255, 255, 255 ), "You have a conflicting addon installed please uninstall ", Color( 255, 122, 0 ), "https://steamcommunity.com/sharedfiles/filedetails/?id=" .. wsid )
+            else
+                print( "[LFS] You have a conflicting addon installed please uninstall https://steamcommunity.com/sharedfiles/filedetails/?id=" .. wsid )
+            end
+        end
+    end
+end )
