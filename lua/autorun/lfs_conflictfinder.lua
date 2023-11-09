23445b4b66c9c196205739534353aee489cdcced
Redox <mail@redox.to>
2023-11-09 14:09:18 +0100

    Add conflicting addons

--- lua/autorun/lfs_conflictfinder.lua
+++ lua/autorun/lfs_conflictfinder.lua
@@ -15,6 +15,45 @@ local conflictingAddons = {
     ["2945559308"] = true, -- Starwars reupload
     ["2888564559"] = true, -- Simfphys and LFS reupload
     ["2984271396"] = true, -- Reupload
+
+    -- Found using https://gmod-archive.cheetah.cat/#/file_relations/
+    -- Reuploads of lfs_basescript.lua and other crucial lfs files
+    ["2829073644"] = true,
+    ["2069109534"] = true,
+    ["2269880345"] = true,
+    ["2161114272"] = true,
+    ["2151185391"] = true,
+    ["2146084653"] = true,
+    ["2707226213"] = true,
+    ["2325614914"] = true,
+    ["2903926541"] = true,
+    ["2894186543"] = true,
+    ["2889133221"] = true,
+    ["2888564559"] = true,
+    ["2888228306"] = true,
+    ["2881549420"] = true,
+    ["2880408560"] = true,
+    ["2878863393"] = true,
+    ["2877653550"] = true,
+    ["2876376550"] = true,
+    ["2873148626"] = true,
+    ["1674191534"] = true,
+    ["1922346958"] = true,
+    ["2786936190"] = true,
+    ["2904112707"] = true,
+    ["2074073623"] = true,
+    ["1933911949"] = true,
+    ["1608143666"] = true,
+    ["2002862434"] = true,
+    ["2869147496"] = true,
+    ["1975234016"] = true,
+    ["1973950819"] = true,
+    ["1997086471"] = true,
+    ["1951516762"] = true,
+    ["2637869858"] = true,
+    ["2819083821"] = true,
+    ["2662591369"] = true,
+    --[""] = true,
 }
 
 local timer_Simple = timer.Simple
